63c8ce80a8be2d9786b95a593893accd65cd68cc
fire8327 <fire83274@gmail.com>
2025-08-11 18:43:00 +0300

    add modal

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,4 +1,19 @@
+/* mobile menu */
 $("#toggler, #overlay").click(() => {
     $("#menu").toggleClass("max-lg:top-0 max-lg:top-full max-lg:-translate-y-full")
     $("#overlay").toggleClass("max-lg:hidden")
+})
+
+
+/* modal */
+$(".openMainModal").each((i, el) => {
+    $(el).click(() => {
+        $("#mainModal").removeClass("hidden")
+        $("#overlayModal").removeClass("hidden")
+    })
+})
+
+$("#closeModal, #overlayModal").click(() => {
+    $("#mainModal").addClass("hidden")
+    $("#overlayModal").addClass("hidden")
 })
